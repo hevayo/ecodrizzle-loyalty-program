2be15f869738a8425c8cdba5138d4eb1bf778d9c
Joseph Fonseka hevayo@gmail.com <user@example.com>
2025-07-29 00:42:20 +0530

    Update login

--- loyalty-app/src/pages/Login.tsx
+++ loyalty-app/src/pages/Login.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { useAuth } from '../contexts/AuthContext'
-import Cookie from 'js-cookie'
 
 const LoginContainer = styled.div`
   min-height: 100vh;
